ea6df9c10cfeb2e9eebf06dbb41af92c96b570a6
Ayoub Bedjou <bedjouayoub@gmail.com>
2026-04-14 13:55:59 +0200

    deploy render

--- quiz_cinema/settings.py
+++ quiz_cinema/settings.py
@@ -63,5 +63,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # 👈 ajouté
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # 👈 ajouté
